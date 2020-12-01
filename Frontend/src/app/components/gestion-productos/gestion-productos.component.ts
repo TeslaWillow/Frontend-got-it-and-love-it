8c1ebf2b888f54c353c30b34db371cb4b7b26859
Héctor Caballero <hectorjosue.caballero@gmail.com>
2020-12-01 01:47:49 -0600

    añadidos el formgroup a formulario de editar productos, hay que ponerle el ID a los formularios para hacer el put sino como lo edita?

--- Frontend/src/app/components/gestion-productos/gestion-productos.component.ts
+++ Frontend/src/app/components/gestion-productos/gestion-productos.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ProductosService, Producto } from '../../services/productos.service';
 import { Categoria, CategoriasService } from '../../services/categorias.service';
@@ -23,83 +23,24 @@ export class GestionProductosComponent implements OnInit {
   @ViewChild('modalVerCategorias') modalVerCategorias;
   @ViewChild('modalCrearProducto') modalCrearProducto;
   @ViewChild('modalEditarProducto') modalEditarProducto;
-
+  @ViewChild('modalEditCategoria') modalEditCategoria;
+  
   constructor(
     private _NgModel:NgbModal,
     private _ProductosService:ProductosService,
     private _CategoriasService:CategoriasService,
-    private fb:FormBuilder,
-    private cd: ChangeDetectorRef
+    private fb:FormBuilder
   ) { 
     this.crearFormularios();
   }
 
   ngOnInit(): void {
-    this.productos = this._ProductosService.getProductos();
+    this.productos = this.GET_Productos();
   }
-
-  /* Validador generico*/
-  isValid(formulario:FormGroup, campo:string){
-    return formulario.get(campo).invalid && formulario.get(campo).touched;
-  }
-
-  unaFuncion(event:any){
-    if(event.target.files && event.target.files.length > 0){
-      const archivo = (event.target as HTMLInputElement).files[0];
-      if(archivo.type.split("/")[0] === "image"){
-        this.formProductos.patchValue({
-          foto: archivo
-        });
-        this.formProductos.get('foto').updateValueAndValidity();
-    
-        //preview
-        const reader = new FileReader();
-        reader.onload = () => {
-          this.imagen = reader.result as string;
-        };
-        reader.readAsDataURL(archivo);
-      }else{
-        this.imagen = null;
-      }
-    }
-  }
-
-  verCategorias(){
-    this.categorias = this._CategoriasService.getCategorias();
-    this._NgModel.open(this.modalVerCategorias, {size: "lg"});
-  }
-
-  crearCategoria(){
-    this._NgModel.open(this.modalCrearCategoria, {size: "lg"});
-  }
-
-  limpiarFormularioProductos(){
-    this.formProductos.reset();
-    this.imagen = null;
-  }
-
-  crearProducto(){
-    this.limpiarFormularioProductos();
-    this.categorias = this._CategoriasService.getCategorias();
-    this._NgModel.open(this.modalCrearProducto, {size: "lg"});
-  }
-
-  editarProducto(_id:number){
-    this.limpiarFormularioProductos();
-    let producto:Producto = this._ProductosService.getProducto(_id);
-    this.categorias = this._CategoriasService.getCategorias();
-    this.formProductos.reset({
-      nombre: producto.nombre,
-      descripcion: producto.descripcion,
-      precio: producto.precio,
-      categoria: producto.categoria,
-      foto: producto.foto
-    });
-    this._NgModel.open(this.modalEditarProducto, {size: "lg"});
-  }
-
+  
   crearFormularios(){
     this.formProductos = this.fb.group({
+      _id: [''],
       nombre: ['', [
         Validators.required,
         Validators.minLength(5),
@@ -123,6 +64,7 @@ export class GestionProductosComponent implements OnInit {
     });
   
     this.formCategorias = this.fb.group({
+      _id: [''],
       nombre: ['', [
         Validators.required,
         Validators.maxLength(100),
@@ -133,8 +75,101 @@ export class GestionProductosComponent implements OnInit {
       ]]
     });
   }
+  /* Validador generico*/
+  isValid(formulario:FormGroup, campo:string){
+    return formulario.get(campo).invalid && formulario.get(campo).touched;
+  }
 
-  guardarProducto(){
+  cargarFoto(event:any){
+    if(event.target.files && event.target.files.length > 0){
+      const archivo = (event.target as HTMLInputElement).files[0];
+      if(archivo.type.split("/")[0] === "image"){
+        this.formProductos.patchValue({
+          foto: archivo
+        });
+        this.formProductos.get('foto').updateValueAndValidity();
+    
+        //preview
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.imagen = reader.result as string;
+        };
+        reader.readAsDataURL(archivo);
+      }else{
+        this.imagen = null;
+      }
+    }
+  }
+
+  verCategorias(){
+    this.categorias = this.GET_Categorias();
+    this._NgModel.open(this.modalVerCategorias, {size: "lg"});
+  }
+
+  crearCategoria(){
+    this.limpiarFormularioCategorias();
+    this._NgModel.open(this.modalCrearCategoria, {size: "lg"});
+  }
+
+  limpiarFormularioProductos(){
+    this.formProductos.reset();
+    this.imagen = null;
+  }
+
+  limpiarFormularioCategorias(){
+    this.formCategorias.reset();
+  }
+
+  crearProducto(){
+    this.limpiarFormularioProductos();
+    this.categorias = this.GET_Categorias();
+    this._NgModel.open(this.modalCrearProducto, {size: "lg"});
+  }
+
+  editarProducto(_id:number){
+    this.limpiarFormularioProductos();
+    let producto:Producto = this.GET_Producto(_id);
+    this.categorias = this.GET_Categorias();
+    this.formProductos.reset({
+      _id: _id,
+      nombre: producto.nombre,
+      descripcion: producto.descripcion,
+      precio: producto.precio,
+      categoria: producto.categoria,
+      foto: producto.foto
+    });
+    this._NgModel.open(this.modalEditarProducto, {size: "lg"});
+  }
+
+  editarCategoria(_id:number){
+    this.limpiarFormularioCategorias();
+    let categoria:Categoria = this.GET_Categoria(_id);
+    this.formCategorias.reset({
+      _id: _id,
+      nombre: categoria.nombre,
+      descripcion: categoria.descripcion
+    });
+    this._NgModel.open(this.modalEditCategoria, {size: "lg"});
+  }
+
+  // Metodos que solicitan informacion. (GET)
+  GET_Productos(){
+    return this._ProductosService.getProductos();
+  }
+
+  GET_Producto(_id:number){
+    return this._ProductosService.getProducto(_id);
+  }
+
+  GET_Categorias(){
+    return this._CategoriasService.getCategorias();
+  }
+
+  GET_Categoria(_id:number){
+    return this._CategoriasService.getCategoria(_id);
+  }
+  // Metodos que se conectan a servicios para hacer el envio de la informacion y recibir respuestas de confirmación.
+  POST_Producto(){
     if(this.formProductos.invalid){
       return Object.values(this.formProductos.controls).forEach(control => {
         control.markAsTouched();
@@ -144,7 +179,17 @@ export class GestionProductosComponent implements OnInit {
     }
   }
 
-  actualizarProducto(){
+  POST_Categoria(){
+    if(this.formCategorias.invalid){
+      return Object.values(this.formCategorias.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }else{
+      console.log("Categoria guardada:", this.formCategorias.value);
+    }
+  }
+  
+  PUT_Producto(){
     if(this.formProductos.invalid){
       return Object.values(this.formProductos.controls).forEach(control => {
         control.markAsTouched();
@@ -154,13 +199,13 @@ export class GestionProductosComponent implements OnInit {
     }
   }
 
-  guardarCategoria(){
+  PUT_Categoria(){
     if(this.formCategorias.invalid){
       return Object.values(this.formCategorias.controls).forEach(control => {
         control.markAsTouched();
       });
     }else{
-      console.log("Categoria guardada:", this.formCategorias.value);
+      console.log("Categoria Actualizada", this.formCategorias.value);
     }
   }
 }
